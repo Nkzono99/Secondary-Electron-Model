4e1864b73b2a61be30a62bae25efef7f7b36492b
Nkzono99 <210x218x@stu.kobe-u.ac.jp>
2025-04-04 15:50:50 +0900

    Add: Added fpm project.

---
+++ src/Secondary-Electron-Model.f90
@@ -0,0 +1,10 @@
+module Secondary_Electron_Model
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Secondary-Electron-Model!"
+  end subroutine say_hello
+end module Secondary_Electron_Model
